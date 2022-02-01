d348bd70dda2596d395c6fa28176f4e5b99fd0c6
corey-rosamond <97810442+corey-rosamond@users.noreply.github.com>
2022-02-01 12:28:28 -0800

    Starting work on the front end interface.

---
+++ frontend/src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+/** Import the components */
+
+import Login from "./components/login.js";
+import ProductList from "./components/product-list.js";
+import Products from "./components/products.js";
+
+
+
+
+
+
+function App()
+{
+    const [user, setUser] = React.useState(null);
+    async function login(user = null)
+    {
+        setUser(user);
+    }
+
+
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+                <a href="/inventory" className="navbar-brand">
+                    &nbsp;The Mystery Market
+                </a>
+                <div className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link to={"/products"} className="nav-link">
+                            Products
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to={"/login"} className="nav-link">
+                            Login
+                        </Link>
+                    </li>
+                </div>
+            </nav>
+
+            <div className="container mt-3">
+                <Switch>
+                    <Route exact path={["/", "/products"]} component={ProductList} />
+                    <Route
+                        path="/products/:id"
+                        render={(props) => (
+                            <Products {...props} user={user} />
+                        )}
+                    />
+                    <Route
+                        path="/login"
+                        render={(props) => (
+                            <Login {...props} login={login} />
+                        )}
+                    />
+                </Switch>
+            </div>
+        </div>
+    );
+}
+
+export default App;
