3a0c20293fce7772221264588838c4cad009dfca
corey-rosamond <97810442+corey-rosamond@users.noreply.github.com>
2022-02-02 14:22:12 -0800

    Refactored the product-list.js to use a class component rather than a function I believe this is a lot cleaner now and easier to understand.

--- frontend/src/components/product-list.js
+++ frontend/src/components/product-list.js
@@ -1,94 +1,150 @@
-import React, {
-    useState,
-    useEffect
-} from "react";
+import React from "react";
 import ProductDataService from "../services/product"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash, faEdit, faEye } from '@fortawesome/fontawesome-free-solid'
-//import { Link } from "react-router-dom"
 
-function ProductList()
+/**
+ * Product List
+ *
+ * This component handles the product list and the search component for the product list.
+ */
+class ProductList extends React.Component
 {
-    const [products, setProducts] = useState([]);
-    const [searchName, setSearchName] = useState("");
+    /**
+     * Constructor
+     *
+     * Pretty standard set the state vars and retrieve the initial product list.
+     * @param props Standard react Props
+     */
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            products: [],
+            searchName: ""
+        }
+        /** Retrieve the initial product list */
+        this.retrieveProducts();
+    }
 
+    /**
+     * On Search Name Change
+     *
+     * This handles when something is typed into the search bar.
+     * @param e Event object
+     */
+    onSearchNameChange(e)
+    {
+        this.setState({ searchName: e.target.value })
+    }
 
-    useEffect(() => {
-        retrieveProducts();
+    /**
+     * On Key Down
+     *
+     * This method is triggered by the on key down event. It checks if
+     * Enter was pressed, if it was it calls the findByName.
+     * @param e Event object
+     */
+    onKeyDown(e)
+    {
+        if(e.key === "Enter")
+        {
+            this.findByName();
+        }
+    }
 
-    }, []);
-
-
-    const retrieveProducts = () => {
+    /**
+     * Retrieve Products
+     *
+     * This method will get the full product list and update the state variable.
+     */
+    retrieveProducts()
+    {
         ProductDataService.getAll()
             .then(response => {
-                setProducts(response.data.products);
+                this.setState({products: response.data.products});
             })
             .catch(e => {
                 console.log(e);
             });
-    };
+    }
 
-    const find = (by, query) => {
+    /**
+     * Find
+     *
+     * This method will find all the products that match the search criteria.
+     * @param by        What field you are searching by
+     * @param query     What to search for
+     */
+    find(by, query)
+    {
         ProductDataService.find(by, query)
             .then(response => {
-                setProducts(response.data.products);
+                this.setState({products: response.data.products});
             })
             .catch(e => {
-               console.log(e);
+                console.log(e);
             });
+    }
+
+    /**
+     * Find By Name
+     *
+     * This method is an interface to the find method it passes the
+     * by value as name and the query will always be the searchName state
+     * value.
+     */
+    findByName()
+    {
+        this.find("name", this.state.searchName);
     };
 
-    const findByName = () => {
-        find("name", searchName);
-    };
+    render()
+    {
+        const viewStyle = {
+            width: "50px",
+            textAlign: "center"
+        };
 
-    const onChangeSearchName = e => {
-        const searchName = e.target.value;
-        setSearchName(searchName);
-    };
+        const editStyle = {
+            width: "50px",
+            textAlign: "center"
+        };
 
-    const viewStyle = {
-        width: "50px",
-        textAlign: "center"
-    };
+        const deleteStyle = {
+            width: "50px",
+            textAlign: "center"
+        };
 
-    const editStyle = {
-        width: "50px",
-        textAlign: "center"
-    };
-
-    const deleteStyle = {
-        width: "50px",
-        textAlign: "center"
-    };
-
-    return (
-        <div>
-            <div className="row pb-1">
-                <div className="input-group col-lg-4">
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Search by name"
-                        value={searchName}
-                        onChange={onChangeSearchName}
-                    />
-                    <div className="input-group-append">
-                        <button
-                            className="btn btn-outline-secondary"
-                            type="button"
-                            onClick={findByName}
-                        >
-                            Search
-                        </button>
+        /**
+         * This is the basic render method called by react.
+         */
+        return (
+            <div>
+                <div className="row pb-1">
+                    <div className="input-group col-lg-4">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Search by name"
+                            onChange={this.onSearchNameChange.bind(this)}
+                            onKeyDown={this.onKeyDown.bind(this)}
+                        />
+                        <div className="input-group-append">
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={this.findByName.bind(this)}
+                            >
+                                Search
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="row">
-                <table className="table table-striped">
-                    <thead>
+                <div className="row">
+                    <table className="table table-striped">
+                        <thead>
                         <tr>
                             <th>View</th>
                             <th>Name</th>
@@ -96,9 +152,9 @@ function ProductList()
                             <th>Edit</th>
                             <th>Delete</th>
                         </tr>
-                    </thead>
-                    <tbody>
-                        {products.map((product) => {
+                        </thead>
+                        <tbody>
+                        {this.state.products.map((product) => {
                             return (
                                 <tr key={product._id.toString()}>
                                     <td style={viewStyle}>
@@ -124,11 +180,12 @@ function ProductList()
                                 </tr>
                             );
                         })}
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default ProductList;
