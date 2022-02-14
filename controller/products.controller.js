9ec3087ff3b2682707b3658aee06d58d718102e1
corey-rosamond <97810442+corey-rosamond@users.noreply.github.com>
2022-02-14 14:25:11 -0800

    Removing old code and swapping the the mongoose model as apposed to the old method.

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -1,4 +1,5 @@
-import ProductsDAO from "../dao/productsDAO.js";
+import ProductsModel from "../model/products.model.js";
+import ErrorResponse from "../utils/error.response.js";
 
 /**
  * Product Controller
@@ -14,34 +15,7 @@ export default class ProductsController
      */
     static async apiGetProducts(req, res, next)
     {
-        const productsPerPage = req.query.productsPerPage ? parseInt(req.query.productsPerPage, 10) : 20;
-        const page = req.query.page ? parseInt(req.query.page, 10) : 0;
 
-        let filters = {};
-        // Filter code goes here
-        if(req.query.name)
-        {
-            filters.name = req.query.name;
-        }
-
-        const {
-            productList,
-            totalNumProducts
-        } = await ProductsDAO.getProducts({
-            filters,
-            page,
-            productsPerPage
-        });
-
-        let response = {
-            products: productList,
-            page: page,
-            filters: filters,
-            entries_per_page: productsPerPage,
-            total_results: totalNumProducts
-        }
-
-        res.json(response);
     }
 
     /**
@@ -53,21 +27,7 @@ export default class ProductsController
      */
     static async apiGetProductById(req, res, next)
     {
-        try
-        {
-            let id = req.params.id || {};
-            let product = await ProductsDAO.getProductById(id);
-            if(!product)
-            {
-                res.status(404).json({error: "not found"});
-                return;
-            }
-            res.json(product);
-        } catch(e)
-        {
-            console.log(`api, ${e}`);
-            res.status(500).json({error: e});
-        }
+
     }
 
     /**
@@ -79,32 +39,7 @@ export default class ProductsController
      */
     static async apiAddProduct(req, res, next)
     {
-        try
-        {
-            const name = req.body.name;
-            const description = req.body.description;
-            const purchase_price = req.body.purchase_price;
-            const our_price = req.body.our_price;
-            const retail_price = req.body.retail_price;
-            const quantity = req.body.quantity;
-            const retail_link = req.body.retail_link;
 
-            const addProductResponse = await ProductsDAO.addProduct(
-                name,
-                description,
-                purchase_price,
-                our_price,
-                retail_price,
-                quantity,
-                retail_link
-            );
-
-            res.json({ status: "success" })
-        } catch (e)
-        {
-            res.status(500)
-                .json({error: e.message});
-        }
     }
 
     /**
@@ -116,44 +51,7 @@ export default class ProductsController
      */
     static async apiUpdateProduct(req, res, next)
     {
-        try
-        {
-            const productId = req.body.id;
-            const name = req.body.name;
-            const description = req.body.description;
-            const our_price = req.body.our_price;
-            const retail_price = req.body.retail_price;
-            const quantity = req.body.quantity;
-            const retail_link = req.body.retail_link;
 
-            const updateResponse = await ProductsDAO.updateProduct(
-                productId,
-                name,
-                description,
-                our_price,
-                retail_price,
-                quantity,
-                retail_link
-            );
-
-            let { error } = updateResponse;
-
-            if(error)
-            {
-                res.status(400).json({error});
-            }
-
-            if(updateResponse.modifiedCount === 0)
-            {
-                throw new Error("Unable to update product!");
-            }
-
-            res.json({ status: "success" });
-        } catch(e)
-        {
-            res.status(500)
-                .json({error: e.message});
-        }
     }
 
     /**
@@ -165,15 +63,6 @@ export default class ProductsController
      */
     static async apiDeleteProduct(req, res, next)
     {
-        try
-        {
-            const productId = req.body.id;
-            const deleteResponse = await ProductsDAO.deleteProduct(productId);
-            res.json({status: "success"});
-        } catch (e)
-        {
-            res.status(500)
-                .json({error: e.message});
-        }
+
     }
 }
